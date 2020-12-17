bb7742419a1f0e0371064881fd6294cdcbdcf3ab
Josh Kowarsky <josh.kowarsky@gmail.com>
2020-12-16 20:34:56 -0800

    Initial Commit

---
+++ TimelineSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimelineSlider
+//
+//  Created by Josh Kowarsky on 12/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimelineSlider/TimelineSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineSliderApp.swift
+//  TimelineSlider
+//
+//  Created by Josh Kowarsky on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct TimelineSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
